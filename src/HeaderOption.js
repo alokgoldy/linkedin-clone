8c80eb109910296455a9bc7606070209ad54ff27
alok-CAW <alok.goldy@cawstdios.com>
2022-06-24 14:38:19 +0530

    changes name

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -11,7 +11,7 @@ function HeaderOption({avatar, title, Icon, onClick}) {
           src="https://www.google.com/url?sa=i&url=https%3A%2F%2Flwlies.com%2Farticles%2Favatar-will-reclaim-its-all-time-box-office-title-with-a-re-release-in-china%2F&psig=AOvVaw05OMgcNhctQB-RBJHKAAGu&ust=1652897864662000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPDgg8SS5_cCFQAAAAAdAAAAABAD"
           className="headerOption__icon"
         >
-          alok
+          alok goldy
         </Avatar>
       )}
       <h3 className="headerOption__title">{title}</h3>
